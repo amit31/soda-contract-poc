626713cecb4f796ed2bcceefa8d6ef2f8255e64b
achumagin <achumagin@hotmail.com>
2023-11-17 23:48:23 +0300

    feat: added table name to data contract filename

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -3,15 +3,16 @@ from data_contract_generator import generate_data_contract
 from run_checks import DataContractScanner
 
 def main():
+    table_name = 'customer_dimension'
     config_path = 'configuration/configuration.yml'
-    data_contract_path = 'data/data_contract.yml'
+    data_contract_path = f"data/{table_name}_data_contract.yml"
+
     vertica_config = read_vertica_connection_config(config_path)
 
     # Create a Vertica connection
     connection = create_vertica_connection(vertica_config)
     print("Successfully connected to Vertica!")
 
-    table_name = 'customer_dimension'
     column_info = get_vertica_table_structure(table_name, connection)
     data_contract_yaml = generate_data_contract(column_info)
 
